9edb8a11c296f94f888c8d4ffddfec28a775fc0c
Ch1traksh <chitraksh.kumar456@gmail.com>
2023-03-24 13:23:37 +0530

    Completed Task-2

--- Task 2/Task2.py
+++ Task 2/Task2.py
@@ -1,2 +1,36 @@
-import cv2
+import cv2 as cv
+import numpy as np
+
+vid = cv.VideoCapture("input.mp4")
+
+fourcc = cv.VideoWriter_fourcc(*'mp4v')
+out = cv.VideoWriter("output.mp4", fourcc, 30.0, (843, 480))
+
+skip_rate = 4
+
+frame_no = 0
+processed_frame_count = 0
+
+while True:
+    ret = vid.grab()
+    if not ret:
+        break
+
+    frame_no += 1
+
+    if(frame_no % skip_rate == 0):
+        processed_frame_count += 1
+
+        status, frame = vid.retrieve()
+
+        frame = cv.resize(frame, (843, 480))
+        out.write(frame)
+
+        cv.imshow("Frame", frame)
+        cv.waitKey(1)
+
+
+vid.release()
+out.release()
+print(frame_no, processed_frame_count)
 
